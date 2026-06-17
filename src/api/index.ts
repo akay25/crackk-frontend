aa4a1432e6957c60820b57d0d4f0c42eb250414b
Ajay M <ajay@shivy.co.in>
2026-06-17 16:22:22 +0530

    Removed unused code

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,20 +1,20 @@
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosError } from "axios";
 
 // Local imports
 const API_TIMEOUT = 30000;
-export const URI = '/api';
+export const URI = "/api";
 const service = axios.create({
   baseURL: URI,
   timeout: API_TIMEOUT,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 // Add request interceptor
 service.interceptors.request.use(
   (config) => {
-    if (config.method && config.method.toUpperCase() !== 'OPTIONS') {
+    if (config.method && config.method.toUpperCase() !== "OPTIONS") {
       // NOTE: Logic to add token
       // const token = localStorage.getItem(LOCALSTORAGE_KEYS.TOKEN);
       // if (token) {
@@ -26,21 +26,7 @@ service.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
-  }
-);
-
-// Add response interceptor
-service.interceptors.response.use(
-  (response) => response,
-  (error: AxiosError) => {
-    // Handle global errors here
-    if (error.response?.status === 401) {
-      // Handle unauthorized access
-      // localStorage.removeItem(LOCALSTORAGE_KEYS.TOKEN);
-      // You might want to redirect to login page here
-    }
-    return Promise.reject(error);
-  }
+  },
 );
 
 export default service;
